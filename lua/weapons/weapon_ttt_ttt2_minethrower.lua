2546f2f1aec41e7184c0c32a4c4a371af4974119
mexikoedi <mexikoedi@users.noreply.github.com>
2021-12-06 23:40:23 +0100

    some small adjustments

--- lua/weapons/weapon_ttt_ttt2_minethrower.lua
+++ lua/weapons/weapon_ttt_ttt2_minethrower.lua
@@ -5,17 +5,16 @@ if SERVER then
     resource.AddFile("materials/vgui/ttt/weapon_minethrower.vmt")
 end
 
---Metadata
 SWEP.PrintName = "Minethrower"
 SWEP.Author = "mexikoedi"
-SWEP.Instructions = "Left Click fires a Combine mine"
---Spawnable w/ spawnmenu
-SWEP.Spawnable = true
+SWEP.Contact = "Steam"
+SWEP.Instructions = "Left click fires a combine mine."
+SWEP.Purpose = "Shoot mines to create traps for your enemies."
+SWEP.Spawnable = false
 SWEP.AdminOnly = false
+SWEP.AdminSpawnable = false
 
---TTT Metadata
 if CLIENT then
-    SWEP.PrintName = "Minethrower"
     SWEP.Slot = 6
     SWEP.ViewModelFlip = false
     SWEP.ViewModelFOV = 60
@@ -24,19 +23,17 @@ if CLIENT then
     SWEP.EquipMenuData = {
         type = "item_weapon",
         name = "Minethrower",
-        desc = "Shoots out combine mines with Left Click!"
+        desc = "Shoot out combine mines with left click!"
     }
 
     SWEP.Icon = "vgui/ttt/weapon_minethrower"
     SWEP.IconLetter = "I"
 end
 
---More info
 SWEP.Base = "weapon_tttbase"
 SWEP.HoldType = "physgun"
 SWEP.Kind = WEAPON_EQUIP
 
---Detectives and Traitors can buy it
 SWEP.CanBuy = {ROLE_DETECTIVE, ROLE_TRAITOR}
 
 SWEP.UseHands = false
@@ -49,23 +46,24 @@ SWEP.Primary.Ammo = "none"
 SWEP.Primary.Delay = 5.0
 SWEP.LimitedStock = true
 SWEP.NoSights = true
+SWEP.AllowDrop = true
+SWEP.AutoSpawnable = false
 
 function SWEP:PrimaryAttack()
-    --Checks whether there is any ammo left.
-    if (self:Clip1() > 0) then
+    if self:Clip1() > 0 then
         self:SetNextPrimaryFire(CurTime() + 0.5)
-        if (CLIENT) then return end
+        if CLIENT then return end
         local ent = ents.Create("combine_mine")
-        if (not IsValid(ent)) then return end
+        if not IsValid(ent) then return end
         ent.origin = "weapon_ttt_ttt2_minethrower"
-        ent:SetPos(self:GetOwner():EyePos() + (self:GetOwner():GetAimVector() * 30))
+        ent:SetPos(self:GetOwner():EyePos() + self:GetOwner():GetAimVector() * 30)
         ent:SetAngles(self:GetOwner():EyeAngles())
         ent:SetOwner(self:GetOwner())
         ent:SetPhysicsAttacker(self:GetOwner())
         ent:Spawn()
         local phys = ent:GetPhysicsObject()
 
-        if (not IsValid(phys)) then
+        if not IsValid(phys) then
             ent:Remove()
 
             return
@@ -81,7 +79,7 @@ end
 
 if SERVER then
     hook.Add("EntityTakeDamage", "TTT2MineThrowerDamage", function(target, dmginfo)
-        if (target:IsPlayer() and dmginfo:IsExplosionDamage() and dmginfo:GetAttacker():GetClass() == "combine_mine" and dmginfo:GetAttacker().origin == "weapon_ttt_ttt2_minethrower") then
+        if target:IsPlayer() and dmginfo:IsExplosionDamage() and dmginfo:GetAttacker():GetClass() == "combine_mine" and dmginfo:GetAttacker().origin == "weapon_ttt_ttt2_minethrower" then
             dmginfo:SetInflictor(ents.Create("weapon_ttt_ttt2_minethrower"))
             dmginfo:SetAttacker(dmginfo:GetAttacker():GetOwner())
         end
